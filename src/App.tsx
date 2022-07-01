68d02a6d8e54927831ee11f3a0b1d818f6ac4abb
Kupparos <kupparos@gmail.com>
2022-07-02 02:13:38 +0700

    work with API

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
-import {FC} from 'react';
-import './App.css';
+import { FC } from "react";
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 const App: FC = () => {
+  
+  const [advice, setAdvice] = useState<string>('Generate a new piece of advice by clicking the dice')
+  
+  type GetAdviceResponse = {
+    data: string;
+  };
+
+  async function getAdvice() {
+    axios.get<GetAdviceResponse>('https://api.adviceslip.com/advice')
+        .then(response => {
+            setAdvice( JSON.stringify(response.data) );
+        });
+  }
+
   return (
     <div className="app">
       <div className="container">
-
+        <div className="advice_block">
+          <div className="number">Advice #100</div>
+          <div className="text">{advice}</div>
+          <div className="dice">
+            <button onClick={getAdvice}>...</button>
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
